b6a825fc0aed4a2842023d184415bb353479c7ae
akshaymu <akshaymu@ICS-LT-66Q1ZD3>
2024-04-26 21:24:13 +0530

    Add project files.

---
+++ Mortgage-Calculator-using-BlazorWASM/wwwroot/pdfGenerator.js
@@ -0,0 +1,15 @@
+﻿
+
+    function generatePDF() {
+        // Select the form element
+        var formElement = document.querySelector('form');
+
+        // Convert the form content to PDF
+        html2pdf(formElement, {
+            margin: 0.5,
+            filename: 'report.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { dpi: 192, letterRendering: true },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        });
+    }
